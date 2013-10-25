c429ed9280a55bf88965eafff9d61138aa353e24
Eduardo L. Cotta <ducotta@gmail.com>
2013-10-25 15:52:35 -0300

    'Reiniciando gits' criando classe SysBUG

---
+++ SysBUG.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+
+public class SysBUG {
+	
+	public Cliente cliente;
+	public Funcionario funcionario;
+	public ArrayList <Conta> contas;
+	
+	
+	
+
+}
---
+++ sysBUG.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+
+public class sysBUG {
+	
+	public Cliente cliente;
+	public Funcionario funcionario;
+	public ArrayList <Conta> contas;
+	
+	
+	
+
+}
