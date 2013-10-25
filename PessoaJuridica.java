194c5b7ed46a487522c8819f03e9b28dcce55217
Eduardo L. Cotta <ducotta@gmail.com>
2013-10-25 15:50:07 -0300

    'Reiniciando gits' criando classe PessoaJuridica sob heranca da classe Cliente e adicionando atributo razaoSocial

--- PessoaJuridica.java
+++ PessoaJuridica.java
@@ -1,4 +1,29 @@
 
-public class PessoaJuridica {
+public class PessoaJuridica extends Cliente{
+	
+	public String cnpj;
+	public String razaoSocial;
 
+	public PessoaJuridica(int senha, String nome, String endereco, String cnpj) {
+		super(senha, nome, endereco);
+		this.cnpj = cnpj;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	
+	
 }
