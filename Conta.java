8d51794b871981839d4df1535f588db273b42504
Eduardo L. Cotta <ducotta@gmail.com>
2013-10-25 15:53:10 -0300

    'Reiniciando gits' criando classe Conta sob heranca da classe SysBUG

---
+++ Conta.java
@@ -0,0 +1,48 @@
+
+public class Conta extends SysBUG{
+	
+	public int numero;
+	public int operacao;
+	public String tipo;
+	public Double saldo;
+	
+	public Conta(int numero, int operacao) {
+		super();
+		this.numero = numero;
+		this.operacao = operacao;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getOperacao() {
+		return operacao;
+	}
+
+	public void setOperacao(int operacao) {
+		this.operacao = operacao;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	
+}
