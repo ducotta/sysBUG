64f6613e93e9556708e0355bd3a011c62e38cae3
Eduardo L. Cotta <ducotta@gmail.com>
2013-10-25 16:02:56 -0300

    Adicionando construtor e getters & setters na classe SysBUG

--- SysBUG.java
+++ SysBUG.java
@@ -5,12 +5,38 @@ public class SysBUG {
 	
 	private Cliente cliente;
 	private Funcionario funcionario;
-	private ArreyList<Conta> conta;
+	private ArrayList<Conta> conta;
 	
-	public Conta pegaConta (int operacao){
-		
+	public SysBUG() {
+		super();
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	public ArrayList<Conta> getConta() {
 		return conta;
 	}
+
+	public void setConta(ArrayList<Conta> conta) {
+		this.conta = conta;
+	}
+	
+	
+	
 	
 
 }
