652605cb439562064c2fb3b935c23218bc60791b
Eduardo L. Cotta <ducotta@gmail.com>
2013-10-25 15:50:54 -0300

    'Reiniciando gits' criando classe PessoaFisica sob heranca da classe Cliente

--- PessoaFisica.java
+++ PessoaFisica.java
@@ -1,4 +1,18 @@
 
-public class PessoaFisica {
+public class PessoaFisica extends Cliente{
 
+	public String cpf;
+
+	public PessoaFisica(int senha, String nome, String endereco, String cpf) {
+		super(senha, nome, endereco);
+		this.cpf = cpf;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 }
